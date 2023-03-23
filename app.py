23884a035db95ddbfa41f34d25d27aaf366ea991
hiroo <1157916142@qq.com>
2023-03-23 15:41:35 +0800

    add database support with Falsk-SQLAlchemy

--- app.py
+++ app.py
@@ -1,26 +1,73 @@
+import os
+
+import click
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 from markupsafe import escape
 
 app = Flask(__name__)
 
-name = 'Hiroo Chen'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+# 在扩展类实例化前加载配置
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+
+db = SQLAlchemy(app)  # 初始化扩展，传入程序实例 app
+
+
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+
+class Movie(db.Model):  # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
+
+
+@app.cli.command()  #注册为命令，可以传入name参数来自定义命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')
+
+
+@app.cli.command()
+def forge():
+    db.create_all()
+
+    user = User(name='Hiroo Chen')
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+    db.session.add(user)
+    for movie in movies:
+        m = Movie(title=movie["title"], year=movie["year"])
+        db.session.add(m)
+    db.session.commit()
+
+    click.echo('DB forge done.')
+
+
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html', user=user, movies=movies)
 
 
 @app.route('/user/<user_name>')
@@ -30,4 +77,3 @@ def user_page(user_name):
     # 这样在返回响应时浏览器就不会把它们当做代码执行。
     return f'<h1>Hello {escape(user_name)}! Welcome to my Film-Watchlist</h1><img ' \
            f'src="http://helloflask.com/totoro.gif">'
-
